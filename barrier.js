bb2b073c17ce07a505ba3feeb1d4cf2e771cf5b7
yuishimimi <42453458+yuishimimi@users.noreply.github.com>
2019-04-19 23:44:34 -1000

    Add files via upload

--- barrier.js
+++ barrier.js
@@ -2,7 +2,7 @@ function barrier(){
   this.x = w + 100;
   this.y =h-280;
   this.leftPull =1;
-  this.velocity = 1;
+  this.velocity = 5;
   this.width = 100;
   this.height = 100;
   this.score = 0;
@@ -18,7 +18,7 @@ function barrier(){
       this.score ++;
       this.x = w+100;
       this.y = h-280;
-      this.velocity += .1;
+      this.velocity += 5;
     }
   }
 }
