c91db7a3b80ae52b999d5dd29c8e6c212a5fc4b9
yuishimimi <42453458+yuishimimi@users.noreply.github.com>
2019-04-19 21:56:16 -1000

    Add files via upload

--- platform.js
+++ platform.js
@@ -1,8 +1,8 @@
 function platform(){
 this.x = 0;
-this.y = h-300;
+this.y = h-250;
 this.show = function(){
 fill(color('green'));
-image(grass,this.x, this.y, w, h-300);
+image(grass,this.x, this.y, w, 200);
 }
 }
